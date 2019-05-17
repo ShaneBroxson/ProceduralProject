c4d0f541642b1d05797ae8e349065707966ab037
Shane Broxson <sbrox.2000.1304@gmail.com>
2019-05-17 11:34:04 -0400

    t

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to Production Line Tracker\n\n\n";
+    std::cout << "Press Associated Key to Continue:" << std::endl;
+    std::cout <<"1) Add Employee Account\n";
+    std::cout <<"2) Add Music Player\n";
+    std::cout <<"3) Add Movie Player\n";
+    std::cout <<"4) Display Production Statistics\n";
+    std::cout <<"5) Exit\n";
+}
