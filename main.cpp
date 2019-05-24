8c3e1f807444a80454034d6b9e0ac3aa4ff581a8
Shane Broxson <sbrox.2000.1304@gmail.com>
2019-05-24 11:51:06 -0400

    Formatting

--- main.cpp
+++ main.cpp
@@ -2,13 +2,23 @@
 #include <iomanip>
 #include <string>
 
+//Author Shane Broxson
+//Made in Summer 2019 for COP2001
+//Add Documentation
+
 //Prototypes
 int userInputMain;
+
 int menu();
+
 void produceItems();
+
 void musicPlayer();
+
 void moviePlayer();
+
 void statistics();
+
 void createAccount();
 
 
@@ -19,7 +29,7 @@ int main() {
     menu();
     //Loop Parameters
     int loop = 0;
-    while(loop!=1) {
+    while (loop != 1) {
         loop++;
         loop--;
         if (userInputMain == 1) {
@@ -54,19 +64,21 @@ int main() {
 
 
 }
-int menu(){
+
+int menu() {
     //Main menu
-    std::cout <<"1) Produce Items\n";
-    std::cout <<"2) Add Music Player\n";
-    std::cout <<"3) Add Movie Player\n";
-    std::cout <<"4) Display Production Statistics\n";
-    std::cout <<"5) Create new employee account\n";
-    std::cout <<"6) Exit\n";
-    std::cin>>userInputMain;
+    std::cout << "1) Produce Items\n";
+    std::cout << "2) Add Music Player\n";
+    std::cout << "3) Add Movie Player\n";
+    std::cout << "4) Display Production Statistics\n";
+    std::cout << "5) Create new employee account\n";
+    std::cout << "6) Exit\n";
+    std::cin >> userInputMain;
     return 0;
 }
-void produceItems(){
-    std::cout<<"Produce Item Stub"<<std::endl;
+
+void produceItems() {
+    std::cout << "Produce Item Stub" << std::endl;
     //Start for Producing Items
     std::cout << "Enter the Manufacturer\n";
     std::string manufacturer;
@@ -84,17 +96,17 @@ void produceItems(){
     std::string itemTypeCode;
     // Audio "MM", Visual "VI", AudioMobile "AM", or VisualMobile "VM".
     std::string choice;
-    if(itemTypeChoice==1){
-        choice="MM";
+    if (itemTypeChoice == 1) {
+        choice = "MM";
     }
-    if(itemTypeChoice==2){
-        choice="VI";
+    if (itemTypeChoice == 2) {
+        choice = "VI";
     }
-    if(itemTypeChoice==3){
-        choice="AM";
+    if (itemTypeChoice == 3) {
+        choice = "AM";
     }
-    if(itemTypeChoice==4){
-        choice="VM";
+    if (itemTypeChoice == 4) {
+        choice = "VM";
     }
 
     std::cout << "Enter the number of items that were produced\n";
@@ -102,22 +114,26 @@ void produceItems(){
     std::cin >> numProduced;
     //Printing serial numbers
     int y = 1;
-    for(int i=numProduced; y <= numProduced; y++)
-    {
-        std::string manReduced = manufacturer.substr(0,3);
-        std::cout << "Production Number: " << y << " Serial Number: " << manReduced<<choice<< std::setfill('0') << std::setw(5) << y << std::endl;
+    for (int i = numProduced; y <= numProduced; y++) {
+        std::string manReduced = manufacturer.substr(0, 3);
+        std::cout << "Production Number: " << y << " Serial Number: " << manReduced << choice << std::setfill('0')
+                  << std::setw(5) << y << std::endl;
     }
 }
-void musicPlayer(){
-    std::cout<<"Music Player Stub"<<std::endl;
+
+void musicPlayer() {
+    std::cout << "Music Player Stub" << std::endl;
 }
-void moviePlayer(){
-    std::cout<<"Movie Player Stub"<<std::endl;
+
+void moviePlayer() {
+    std::cout << "Movie Player Stub" << std::endl;
 }
-void statistics(){
-    std::cout<<"Statistics Stub"<<std::endl;
+
+void statistics() {
+    std::cout << "Statistics Stub" << std::endl;
 }
-void createAccount(){
-    std::cout<<"Create Employee Account Stub"<<std::endl;
+
+void createAccount() {
+    std::cout << "Create Employee Account Stub" << std::endl;
 }
 
