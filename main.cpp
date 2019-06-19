51f2d5a3f37066d0e68e0bd4cdfc71eec7ca76af
Shane Broxson <sbrox.2000.1304@gmail.com>
2019-06-19 18:55:10 -0400

    Employee Accounts Added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <vector>
 #include <sstream>
+#include <cctype>
 /** @file main.cpp
  *  @brief Main file.
  *  Saves 3 text documents storing data used in program.
@@ -34,9 +35,14 @@ void sortProductionList(std::vector<int> &, std::vector<std::string> &, std::vec
 void produceStatistics(std::vector<int> &, std::vector<std::string> &, std::vector<std::string> &,
                        std::vector<std::string> &, std::vector<std::string> &, std::vector<std::string> &);
 
-void employeeLogin();
+void createEmployeeAccount(std::vector<std::string> &, std::vector<std::string> &, std::vector<std::string> &);
 
-void addEmployeeAccount();
+void encrypt_password(std::vector<std::string> &, std::vector<std::string> &);
+
+void encrypt_test_password(std::vector<std::string> &);
+
+bool employee_login(std::vector<std::string> &, std::vector<std::string> &, std::vector<std::string> &,
+                    std::vector<std::string> &);
 
 int main() {
     ///********* Vectors for ProductLine*********
@@ -58,6 +64,14 @@ int main() {
     std::vector<std::string> serialNumbers;
 //parallel vector to store production number
     std::vector<int> productionNumbers;
+    ///********* Vector for Emp Account*********
+    std::vector<std::string> password;
+    std::vector<std::string> userName;
+    ///********* Vector for Password Testing****
+    //std::vector<std::string> testPassword;
+    std::vector<std::string> password_temp;
+    ///********* Vector for UserName Testing****
+    std::vector<std::string> testUserName;
 
     std::string line;
     //file to string and string to vector
@@ -77,6 +91,25 @@ int main() {
 
     }
     catalog.close();
+    //if catalog does not exist creates catalog and puts in sample products
+    if (productLineManufacturers.size() == 0) {
+        std::ofstream populateCatalog("Catalog.csv");
+        populateCatalog << "Microsoft,Zune,AM" << std::endl;
+        populateCatalog << "Apple,iPod,AM" << std::endl;
+        populateCatalog << "Sylvania,SDVD1187,VM" << std::endl;
+        populateCatalog.close();
+        while (getline(catalog, line)) {
+            std::stringstream ss(line);
+            getline(ss, manufacturer, ',');
+            getline(ss, prodName, ',');
+            getline(ss, itemTypeCode);
+            productLineManufacturers.push_back(manufacturer);
+            productLineNames.push_back(prodName);
+            productLineItemTypes.push_back(itemTypeCode);
+
+        }
+    }
+
 
     //file to string and string to vector
     //production list
@@ -99,15 +132,50 @@ int main() {
         serialNumbers.push_back(serialNum);
 
         // std::cout<<itemTypeCode<<std::endl;
-
-
     }
     productionList.close();
+
+    //file to string and string to vector
+    //Employee Accounts
+    std::ifstream employeeAccounts("employee_accounts.csv");
+    std::string pass;
+    std::string user;
+    while (getline(employeeAccounts, line)) {
+        std::stringstream ss(line);
+        getline(ss, user, ',');
+        getline(ss, pass);
+        password.push_back(pass);
+        userName.push_back(user);
+    }
+    //If file does not exist, adds file and admin account
+    //user is admin
+    //pass is Adm1n
+    if (userName.size() == 0) {
+        std::string adminName = "admin";
+        std::string adminPass = "Dgp4q";
+        std::ofstream populateEmp("employee_accounts.csv");
+        populateEmp << adminName << "," << adminPass << std::endl;
+        populateEmp.close();
+        std::ifstream employeeAccounts("employee_accounts.csv");
+        std::string pass;
+        std::string user;
+        while (getline(employeeAccounts, line)) {
+            std::stringstream ss(line);
+            getline(ss, user, ',');
+            getline(ss, pass);
+            password.push_back(pass);
+            userName.push_back(user);
+        }
+    }
 //    for(int i=0; i<serialNumbers.size();i++){
 //        std::cout<<serialNumbers[i]<<std::endl;
 //    }
     resetAndRepopulateVectors(productLineManufacturers, productLineNames, productLineItemTypes, productionNumbers,
                               productionManufacturers, productionNames, productionItemTypes, serialNumbers);
+
+    ///*************************LOGIN*********************************
+    //loops until correct login is inputted
+    bool logged_in = false;
 
     //Welcome greeting
     std::cout << "Production Line Tracker\n" << std::endl;
@@ -120,26 +188,46 @@ int main() {
     while (continueProgram) {
         userMenuChoice = displayMenuAndGetChoice();
         if (userMenuChoice == 1) {
-            produceItems(productLineManufacturers, productLineNames, productLineItemTypes, productionNumbers,
-                         productionManufacturers, productionNames, productionItemTypes, serialNumbers);
-            resetAndRepopulateVectors(productLineManufacturers, productLineNames, productLineItemTypes,
-                                      productionNumbers,
-                                      productionManufacturers, productionNames, productionItemTypes, serialNumbers);
+            if (logged_in == true) {
+                produceItems(productLineManufacturers, productLineNames, productLineItemTypes, productionNumbers,
+                             productionManufacturers, productionNames, productionItemTypes, serialNumbers);
+                resetAndRepopulateVectors(productLineManufacturers, productLineNames, productLineItemTypes,
+                                          productionNumbers,
+                                          productionManufacturers, productionNames, productionItemTypes, serialNumbers);
+            } else { std::cout << "Must be logged in to Access!" << std::endl; }
             system("pause");
         } else if (userMenuChoice == 2) {
-            addNewItems(productLineManufacturers, productLineNames, productLineItemTypes);
-            resetAndRepopulateVectors(productLineManufacturers, productLineNames, productLineItemTypes,
-                                      productionNumbers,
-                                      productionManufacturers, productionNames, productionItemTypes, serialNumbers);
+            if (logged_in == true) {
+                addNewItems(productLineManufacturers, productLineNames, productLineItemTypes);
+                resetAndRepopulateVectors(productLineManufacturers, productLineNames, productLineItemTypes,
+                                          productionNumbers,
+                                          productionManufacturers, productionNames, productionItemTypes, serialNumbers);
+            } else { std::cout << "Must be logged in to Access!" << std::endl; }
         } else if (userMenuChoice == 3) {
-            produceStatistics(productionNumbers,
-                              productionManufacturers, productionNames, productionItemTypes, serialNumbers,
-                              productLineItemTypes);
+            if (logged_in == true) {
+                produceStatistics(productionNumbers,
+                                  productionManufacturers, productionNames, productionItemTypes, serialNumbers,
+                                  productLineItemTypes);
+            } else { std::cout << "Must be logged in to Access!" << std::endl; }
         } else if (userMenuChoice == 4) {
-//            empAccount();
+            while (logged_in == false) {
+                std::cout << "Please Login to Access System" << std::endl;
+                logged_in = employee_login(password, userName, password_temp, testUserName);
+            }
+            //login status is changed
+            if (logged_in == true) {
+                std::cout << "Logged In" << std::endl;
+            }
         } else if (userMenuChoice == 5) {
-//            createAccount();
+            createEmployeeAccount(password, userName, password_temp);
+            encrypt_password(password, password_temp);
+
         } else if (userMenuChoice == 6) {
+            if (logged_in == true) {
+                logged_in = false;
+                std::cout << "You have been logged out." << std::endl;
+            } else { std::cout << "No account to log out of..." << std::endl; }
+        } else if (userMenuChoice == 7) {
             continueProgram = false;
         } else {
             std::cout << "You have entered an invalid number, please try again.\n\n" << std::endl;
@@ -154,9 +242,10 @@ int displayMenuAndGetChoice() {
     std::cout << "1) Produce Items\n";
     std::cout << "2) Add New Items\n";
     std::cout << "3) Production Statistics\n";
-    std::cout << "4) Employee Account\n";
-    std::cout << "5) Add Employee Account\n";
-    std::cout << "6) Exit Program\n";
+    std::cout << "4) Login to Employee Account\n";
+    std::cout << "5) Create Employee Account\n";
+    std::cout << "6) Logout\n";
+    std::cout << "7) Exit Program\n";
     int userMenuChoice;
     std::cin >> userMenuChoice;
     return userMenuChoice;
@@ -444,11 +533,11 @@ void produceStatistics(std::vector<int> &productionNumbers, std::vector<std::str
         case 4: {
             //display entire production log
             for (int outputCount = 0; outputCount < productionItemTypes.size(); outputCount++) {
-                    std::cout << productionNumbers[outputCount] << ". Manufacturer: '"
-                              << productionManufacturers[outputCount] << "' Item Type: '"
-                              << productionItemTypes[outputCount]
-                              << "' Product Name: '" << productionNames[outputCount] << "' Serial Number: '" <<
-                              serialNumbers[outputCount] << "'" << std::endl;
+                std::cout << productionNumbers[outputCount] << ". Manufacturer: '"
+                          << productionManufacturers[outputCount] << "' Item Type: '"
+                          << productionItemTypes[outputCount]
+                          << "' Product Name: '" << productionNames[outputCount] << "' Serial Number: '" <<
+                          serialNumbers[outputCount] << "'" << std::endl;
             }
             break;
         }
@@ -458,8 +547,8 @@ void produceStatistics(std::vector<int> &productionNumbers, std::vector<std::str
             std::string serialInput;
             std::cin >> serialInput;
             int ifPrinted = 0;
-            for (int serialCount = 0; serialCount < serialNumbers.size(); serialCount++){
-                if(serialNumbers[serialCount] == serialInput){
+            for (int serialCount = 0; serialCount < serialNumbers.size(); serialCount++) {
+                if (serialNumbers[serialCount] == serialInput) {
                     std::cout << productionNumbers[serialCount] << ". Manufacturer: '"
                               << productionManufacturers[serialCount] << "' Item Type: '"
                               << productionItemTypes[serialCount]
@@ -467,7 +556,7 @@ void produceStatistics(std::vector<int> &productionNumbers, std::vector<std::str
                     ifPrinted++;
                 }
             }
-            if(ifPrinted == 0){
+            if (ifPrinted == 0) {
                 std::cout << "That Serial Number Could Not Be Found." << std::endl;
             }
             break;
@@ -478,10 +567,139 @@ void produceStatistics(std::vector<int> &productionNumbers, std::vector<std::str
     }
 }
 
-void employeeLogin() {
+void createEmployeeAccount(std::vector<std::string> &password, std::vector<std::string> &userName,
+                           std::vector<std::string> &password_temp) {
+    std::cout << "Enter employee's first name:\n";
+    std::string first_name;
+    std::cin >> first_name;
+    std::cout << "Enter employee's last name:\n";
+    std::string last_name;
+    std::cin >> last_name;
+    std::string user_name;
+
+    // create user name
+    char user_first = first_name[0];
+    std::string user_last = last_name;
+    user_name = user_first + user_last;
+    std::transform(user_name.begin(), user_name.end(), user_name.begin(), ::tolower);
+    std::string pass;
+
+
+    std::cout << "Your generated user name: " + user_name + "\n";
+
+    //check if password entered is valid
+    bool valid;
+    std::cout << "Enter employee's password:\n";
+    std::cout << "Must contain a number and lowercase and uppercase letters.\n";
+
+    int count = 0;
+
+    std::cin >> pass;
+    // code to check if valid
+    int uppercaseCount = 0;
+    int lowercaseCount = 0;
+    int digitCount = 0;
+    for (count = 0; count < pass.size(); count++) {
+        if (isupper(pass[count])) {
+            uppercaseCount = 1;
+        }
+        if (islower(pass[count])) {
+            lowercaseCount = 1;
+        }
+        if (isdigit(pass[count])) {
+            digitCount = 1;
+        }
+    }
+    if (uppercaseCount != 0 && lowercaseCount != 0 && digitCount != 0) {
+        valid = true;
+    } else {
+        valid = false;
+    }
+
+
+    if (valid) {
+        std::cout << "Valid Password" << std::endl;
+        password_temp.push_back(pass);
+        userName.push_back(user_name);
+        std::ofstream employeeAccounts;
+        employeeAccounts.open("employee_accounts.csv", std::ios_base::app);
+        employeeAccounts << user_name << ",";
+        employeeAccounts.close();
+        /// save both to file
+    } else {
+        std::cout << "Invalid Password" << std::endl;
+    }
 
 }
 
-void addEmployeeAccount() {
+///ENCRYPTS PASSWORD
+void encrypt_password(std::vector<std::string> &password, std::vector<std::string> &password_temp) {
 
+    std::string pass = password_temp[0];
+    password_temp.pop_back();
+    std::string endPass;
+    for (int count = 0; count < pass.size(); count++) {
+        char first_char = pass[count];
+        first_char += 3;
+        char new_first_char = first_char;
+        endPass += new_first_char;
+    }
+    password.push_back(endPass);
+
+    std::ofstream employeeAccounts;
+    employeeAccounts.open("employee_accounts.csv", std::ios_base::app);
+    employeeAccounts << endPass << std::endl;
+    employeeAccounts.close();
+}
+
+void encrypt_test_password(std::vector<std::string> &password_temp) {
+
+    std::string pass = password_temp[0];
+    password_temp.pop_back();
+    std::string endPass;
+    for (int count = 0; count < pass.size(); count++) {
+        char first_char = pass[count];
+        first_char += 3;
+        char new_first_char = first_char;
+        endPass += new_first_char;
+
+    }
+    password_temp.push_back(endPass);
+}
+
+bool employee_login(std::vector<std::string> &password, std::vector<std::string> &userName,
+                    std::vector<std::string> &password_temp, std::vector<std::string> &testUserName) {
+    std::string userName_temp;
+    std::cout << "Enter Username: " << std::endl;
+    std::cin >> userName_temp;
+    testUserName.push_back(userName_temp);
+    bool user_found = false;
+    bool logged_in = false;
+    for (int count = 0; count < userName.size(); count++) {
+        if (testUserName[0] == userName[count]) {
+            testUserName.pop_back();
+            std::cout << "Enter Password: " << std::endl;
+            std::string test_pass;
+            std::cin >> test_pass;
+            password_temp.push_back(test_pass);
+            encrypt_test_password(password_temp);
+            user_found = true;
+            if (password_temp[0] == password[count]) {
+                std::cout << "Login Success: ";
+                password_temp.pop_back();
+                logged_in = true;
+                break;
+            } else {
+                std::cout << "Incorrect Password!" << std::endl;
+                password_temp.pop_back();
+                break;
+            }
+        }
+    }
+    if (user_found == false) {
+        std::cout << "User was not found!" << std::endl;
+        testUserName.pop_back();
+    }
+
+    return logged_in;
 }
